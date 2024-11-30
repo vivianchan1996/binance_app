afa022578c265a469f54bc90429537d22efe3797
vivianchan1996 <viivian-chan@hotmail.com>
2024-11-30 18:37:56 +0800

    Feature - Create response class for Binance and Houbi

---
+++ src/main/java/com/crypto/model/BinanceCrypto.java
@@ -0,0 +1,31 @@
+package com.crypto.model;
+
+public class BinanceCrypto {
+    private String symbol;
+    private String bidPrice;
+    private String askPrice;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getBidPrice() {
+        return bidPrice;
+    }
+
+    public void setBidPrice(String bidPrice) {
+        this.bidPrice = bidPrice;
+    }
+
+    public String getAskPrice() {
+        return askPrice;
+    }
+
+    public void setAskPrice(String askPrice) {
+        this.askPrice = askPrice;
+    }
+}
