a3efb2c62629ad609aee36c5f053a12fba90fa56
vivianchan1996 <viivian-chan@hotmail.com>
2024-11-30 21:00:00 +0800

    Feature - Create API to fetch latest best aggregated price by symbol

---
+++ src/main/java/com/crypto/controller/PriceController.java
@@ -0,0 +1,24 @@
+package com.crypto.controller;
+
+import com.crypto.model.TradingPrice;
+import com.crypto.repository.TradingPriceRepo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/price")
+public class PriceController {
+    private final TradingPriceRepo tradingPriceRepo;
+
+    public PriceController(TradingPriceRepo tradingPriceRepo) {
+        this.tradingPriceRepo = tradingPriceRepo;
+    }
+
+    @GetMapping("/latest")
+    public ResponseEntity<TradingPrice> getLatestPrice(String symbol) {
+        TradingPrice price = tradingPriceRepo.findLatestTradingPriceByCrypto(symbol);
+        return ResponseEntity.ok(price);
+    }
+}
