cade5d55c5451e8aa52c58e3b5a08a864470bb3e
vivianchan1996 <viivian-chan@hotmail.com>
2024-11-30 18:25:49 +0800

    Feature - Import rest template from spring framework

--- src/main/java/com/crypto/service/PriceAggregationService.java
+++ src/main/java/com/crypto/service/PriceAggregationService.java
@@ -3,13 +3,17 @@ package com.crypto.service;
 import com.crypto.model.TradingPrice;
 import com.crypto.repository.TradingPriceRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class PriceAggregationService {
 
     private TradingPriceRepo tradingPriceRepo;
+    private final RestTemplate restTemplate;
 
     public PriceAggregationService(TradingPriceRepo tradingPriceRepo) {
         this.tradingPriceRepo = tradingPriceRepo;
+        this.restTemplate = new RestTemplate();
     }
 }
